c933e62e3b3ba8201098bc50a865f213eea80346
Jarkko Saarinen <jsaarinen92@gmail.com>
2018-09-01 14:48:33 +0300

    Update dependencies, incl. AndroidX & make them use variables instead of raw strings

--- app/src/main/java/fi/jara/thesis/thesisnative/MainActivity.kt
+++ app/src/main/java/fi/jara/thesis/thesisnative/MainActivity.kt
@@ -1,7 +1,7 @@
 package fi.jara.thesis.thesisnative
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 

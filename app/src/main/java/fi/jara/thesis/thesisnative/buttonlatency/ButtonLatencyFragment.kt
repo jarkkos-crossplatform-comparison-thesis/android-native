16c4a03c94a42b933273d2dd1ea2d1f060504604
Jarkko Saarinen <jsaarinen92@gmail.com>
2018-11-03 21:46:52 +0200

    Implement button latency test

--- app/src/main/java/fi/jara/thesis/thesisnative/buttonlatency/ButtonLatencyFragment.kt
+++ app/src/main/java/fi/jara/thesis/thesisnative/buttonlatency/ButtonLatencyFragment.kt
@@ -6,9 +6,44 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import fi.jara.thesis.thesisnative.R
+import kotlinx.android.synthetic.main.test_button_latency.*
 
 class ButtonLatencyFragment: Fragment() {
+    private var numBtnClicked = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        numBtnClicked = savedInstanceState?.getInt(EXTRA_NUM_BUTTON_CLICKED, 0) ?: 0
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.test_not_implemented, container, false)
+        return inflater.inflate(R.layout.test_button_latency, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        num_btn_clicked_text.text = numBtnClicked.toString()
+
+        increment_counter_button.setOnClickListener { incrementCounter() }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        increment_counter_button.setOnClickListener(null)
+    }
+
+    private fun incrementCounter() {
+        numBtnClicked++
+        num_btn_clicked_text.text = numBtnClicked.toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(EXTRA_NUM_BUTTON_CLICKED, numBtnClicked)
+    }
+
+    companion object {
+        const val EXTRA_NUM_BUTTON_CLICKED = "num_btn_clicked"
     }
 }
